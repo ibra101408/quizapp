820da97b3c738d816ccca37e2adccade1e9f17e2
andrei.pugatsov <andrei.pugatsov@gmail.com>
2026-03-15 16:44:29 +0200

    (feat)QUIZ-12 : implemented basic Liquibase script with basic seeding

--- src/main/java/com/team35/quizapp/config/LiquibaseConfig.java
+++ src/main/java/com/team35/quizapp/config/LiquibaseConfig.java
@@ -1,18 +1,32 @@
 package com.team35.quizapp.config;
 
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class LiquibaseConfig {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${DB_ADMIN_USERNAME:postgres}")
+    private String adminUsername;
+
+    @Value("${DB_ADMIN_PASSWORD:postgres}")
+    private String adminPassword;
+
     @Bean
-    public SpringLiquibase liquibase(DataSource dataSource) {
+    public SpringLiquibase liquibase() {
+        DriverManagerDataSource adminDataSource = new DriverManagerDataSource();
+        adminDataSource.setUrl(url);
+        adminDataSource.setUsername(adminUsername);
+        adminDataSource.setPassword(adminPassword);
+
         SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
+        liquibase.setDataSource(adminDataSource);
         liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
         liquibase.setContexts("dev");
         return liquibase;
